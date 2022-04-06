cf3fc78b3ef8193fe7ab655d8df75240f4cd14e7
ZenRool <lorenzopapperini@hotmail.it>
2022-04-06 13:23:54 +0200

    Inserimento colore, con relativo cambio dello sfondo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 const name1 = prompt ("Come ti chiami?");
 const lastname1 = prompt ("Come fai di cognome?");
 document.getElementById("title").innerHTML =`Ciao ${name1} ${lastname1}!`;
+const color1 = prompt ("Qual è il tuo colore preferito?");
+// se scrive un colore base css cambia il colore del background
+document.body.style.backgroundColor = color1;
