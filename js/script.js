40dc443f72e2d1ab26a98847136c7997394f1790
ZenRool <lorenzopapperini@hotmail.it>
2022-04-06 13:28:44 +0200

    Stampa la password nel paragrafo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const name1 = prompt ("Come ti chiami?");
 const lastname1 = prompt ("Come fai di cognome?");
-document.getElementById("title").innerHTML =`Ciao ${name1} ${lastname1}!`;
+document.getElementById("title").innerHTML = `Ciao ${name1} ${lastname1}!`;
 const color1 = prompt ("Qual è il tuo colore preferito?");
 // se scrive un colore base css cambia il colore del background
 document.body.style.backgroundColor = color1;
+document.getElementById("psw").innerHTML = `La tua password super sicura è ${name1}${lastname1}${color1}21`;
